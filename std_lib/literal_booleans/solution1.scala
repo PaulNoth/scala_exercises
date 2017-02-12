cfc346981c6f7d97dd0527daf436e32ce3c87127
Paul P <paulp@hotmail.sk>
2017-02-12 11:06:05 +0100

    Literal Booleans

---
+++ std_lib/literal_booleans/solution1.scala
@@ -0,0 +1,14 @@
+// Boolean literals are either true or false, using the true or false keyword
+
+val a = true
+val b = false
+val c = 1 > 2
+val d = 1 < 2
+val e = a == c
+val f = b == d
+a should be(true)
+b should be(false)
+c should be(false)
+d should be(true)
+e should be(false)
+f should be(false)
