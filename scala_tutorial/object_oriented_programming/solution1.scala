6045635bdd49c0a18d26b847fecc477cee7938eb
Pavol Pidanič <paulp@hotmail.sk>
2017-02-23 14:18:32 +0100

    added missing solution

--- scala_tutorial/object_oriented_programming/solution1.scala
+++ scala_tutorial/object_oriented_programming/solution1.scala
@@ -1,25 +1,9 @@
-// The following Reducer abstract class defines how to reduce a list of values into a single value
-// by starting with an initial value and combining it with each element of the list:
+// DYNAMIC BINDING
 
-abstract class Reducer(init: Int) {
-  def combine(x: Int, y: Int): Int
-  def reduce(xs: List[Int]): Int =
-    xs match {
-      case Nil => init
-      case y :: ys => combine(y, reduce(ys))
-    }
-}
+// Object-oriented languages (including Scala) implement dynamic method dispatch.
 
-object Product extends Reducer(1) {
-  def combine(x: Int, y: Int): Int = x * y
-}
+// This means that the code invoked by a method call depends on the runtime type of the object that contains the method.
 
-object Sum extends Reducer(0) {
-  def combine(x: Int, y: Int): Int = x + y
-}
+Empty contains 1 shouldBe false
 
-val nums = List(1, 2, 3, 4)
-
-Product.reduce(nums) shouldBe 24
-
-Sum.reduce(nums) shouldBe 10
+new NonEmpty(7, Empty, Empty) contains 7 shouldBe true
